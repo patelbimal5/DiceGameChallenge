9907f6812f1a6083f96a31a8f0a77f94f57e7609
patelbimal5 <68713222+patelbimal5@users.noreply.github.com>
2022-02-25 18:24:23 -0500

    adding button and prompt

--- index.js
+++ index.js
@@ -4,21 +4,26 @@ var randomNumber2 = Math.floor(Math.random()*6) + 1;
 var randomDiceImage = "images/dice" + randomNumber1 + ".png";
 var randomDiceImage2 = "images/dice" + randomNumber2 + ".png";
 
+var player1 = prompt("Please enter player1 name");
+var player2 = prompt("Please enter player2 name");
+
+document.querySelectorAll("p")[0].innerHTML=player1;
+document.querySelectorAll("p")[1].innerHTML=player2;
 
 document.querySelectorAll("img")[0].setAttribute("src", randomDiceImage);
 document.querySelectorAll("img")[1].setAttribute("src", randomDiceImage2);
 
 if(randomNumber1 > randomNumber2)
 {
-  document.querySelector("h1").innerHTML="Player1 Won!";
+  document.querySelector("h2").innerHTML="" + player1 + " Won!";
 }
 else if (randomNumber1 == randomNumber2)
 {
-  document.querySelector("h1").innerHTML="Opps! It is Draw";
+  document.querySelector("h2").innerHTML="Opps! It is Draw";
 }
 else
 {
-  document.querySelector("h1").innerHTML="Player2 is Won";
+  document.querySelector("h2").innerHTML="Player2 Won";
 }
 
 //alert(randomNumber1);
