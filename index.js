9f01d66349e7568a5e4ace963a8750636a74be1f
patelbimal5 <68713222+patelbimal5@users.noreply.github.com>
2022-02-25 17:51:02 -0500

    adding images and setting js scritpts and img styles

---
+++ index.js
@@ -0,0 +1,8 @@
+var randomNumber1 = Math.floor(Math.random()*6) + 1;
+var randomNumber2 = Math.floor(Math.random()*6) + 1;
+
+var randomDiceImage = "dice" + randomNumber1 + ".png";
+
+document.getElementById(h1tag).innerHTML="raadfkdjfdj";
+
+document.getElementByClassName("img1").innerHTML = 'images/dice2.png';
